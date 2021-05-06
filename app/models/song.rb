5a24a5b1a649cc9eb86da4637c720a1e8c01666f
sluongo718 <sluongo718@yahoo.com>
2021-05-05 22:53:35 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    artist = self.artist.name
+  end
+
+  def artist_song
+    art = "#{artist_name} - #{self.title}"
+  end
 end
